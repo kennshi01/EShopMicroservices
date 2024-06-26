7f74c10a75eb7157dead8a8254e93169b03d4456
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-06-26 13:38:16 +0300

    Added Carter to BuildingBlocks.csproj
    Some documentation of CQRS and MediatR

--- src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Product/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,14 @@
 namespace Catalog.API.Product.CreateProduct;
 
+public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
 public class CreateProductEndpoint
 {
 }
