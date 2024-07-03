7aee0d27a62852c878ffc8190d3d20ca8cbbfbbc
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-03 11:28:24 +0300

    FluentValidation Initial setup. Implementation throw MediatR pipelines

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,20 @@
-using Weasel.Core;
+using BuildingBlocks.Behaviors;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var assembly = typeof(Program).Assembly;
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
 builder.Services.AddCarter();
-builder.Services.AddMediatR(config => { config.RegisterServicesFromAssemblies(typeof(Program).Assembly); });
+
+builder.Services.AddValidatorsFromAssembly(assembly);
+
 builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); })
     .UseLightweightSessions(); // There a 3 type's of session, this one is chosen for some purpose :)
 
