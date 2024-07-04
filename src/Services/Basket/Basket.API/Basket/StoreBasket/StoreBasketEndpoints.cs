987718b472c6d2349c4b7f42bce0942b846780ca
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-04 15:32:47 +0300

    Develop Store Basket Endpoint with Minimal Apis and Carter

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketEndpoints.cs
@@ -1,6 +1,27 @@
 namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketEndpoints
+public record StoreBaskerRequest(ShoppingCart Cart);
+
+public record StoreBasketResponse(string UserName);
+
+public class StoreBasketEndpoints : ICarterModule
 {
-    
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/basket", async (StoreBaskerRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<StoreBasketCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<StoreBasketResponse>();
+            
+            return Results.Created($"/basket/{response.UserName}", response);
+        })
+        .WithName("StoreBasket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Store Basket")
+        .WithDescription("Store Basket");
+    }
 }
