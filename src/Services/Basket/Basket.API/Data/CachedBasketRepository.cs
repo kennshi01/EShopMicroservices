c6af3d458e8453b3a67db5d6a68fd7e54b9ea36a
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-05 10:54:34 +0300

    Register Decorated CachedBasketRepository and StackExchangeRedis with Scrutor

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Basket.API.Data;
+
+public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache) : IBasketRepository
+{
+    public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        // Check if basket persists in cache return from cache
+        var cachedBasket = await cache.GetStringAsync(userName, cancellationToken);
+        if (!string.IsNullOrEmpty(cachedBasket))
+            return JsonSerializer.Deserialize<ShoppingCart>(cachedBasket)!;
+        
+        // Else perform a get operation from db
+        var basket = await repository.GetBasket(userName, cancellationToken);
+        
+        // Store in cache data for future retrieves
+        await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+        return basket;
+    }
+
+    public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken = default)
+    {
+        await repository.StoreBasket(basket, cancellationToken);
+        
+        // store in cache
+        await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+        return basket;
+    }
+
+    public async Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+    {
+        await repository.DeleteBasket(userName, cancellationToken);
+
+        await cache.RemoveAsync(userName, cancellationToken);
+
+        return true;
+    }
+}
