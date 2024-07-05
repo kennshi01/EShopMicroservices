cd14ecefbe71bfca2cfefc4c32a7542c69a0f0fd
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-05 09:32:35 +0300

    Exception Handling

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
 using Marten;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,10 +22,12 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
