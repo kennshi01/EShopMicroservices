a8a63264380409ea289eb0551e2afe61f6308b05
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-05 16:30:24 +0300

    Develop DiscountService class to Implement Grpc Proto Service Methods

---
+++ src/Services/Discount/Discount.grpc/Services/DiscountService.cs
@@ -0,0 +1,28 @@
+using Discount.Grpc;
+using Grpc.Core;
+
+namespace Discount.grpc.Services;
+
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        // TODO: GetDiscount from DB
+        return base.GetDiscount(request, context);
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+}
