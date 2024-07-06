18c41450282c3b2e68e3a11918683995e731ece0
ebusuioc <edwardbs876@gmail.com>
2024-07-06 20:07:47 +0300

    Entity Framework Core and SQLite Database Connection in Discount Microservices

---
+++ src/Services/Discount/Discount.grpc/Data/DiscountContext.cs
@@ -0,0 +1,13 @@
+﻿using Discount.grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) : base(options)
+    {
+    }
+}
