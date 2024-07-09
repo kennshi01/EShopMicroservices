24debb9454e0ab1223932f8002e3c2af375df727
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-09 15:07:39 +0300

    Develop ValueObjects (Address, Payment) DDD Rich-domain model Entity

--- src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -1,6 +1,16 @@
 namespace Ordering.Domain.ValueObjects;
 
-public record OrderName()
+public record OrderName
 {
+    private const int DefaultLength = 5;
     public string Value { get; }
+    private OrderName(string value) => Value = value;
+    
+    public static OrderName Of(string value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        ArgumentOutOfRangeException.ThrowIfNotEqual(value.Length, DefaultLength);
+
+        return new OrderName(value);
+    }
 }
