84fb9688f99d204efd23902f2bfeec647322e048
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-09 16:47:43 +0300

    Mapping DDD Objects to EF Entities - Develop EF Core Entity Configurations

--- src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -9,7 +9,7 @@ public record CustomerId
 
     public Guid Value { get; }
 
-    private static CustomerId Of(Guid value)
+    public static CustomerId Of(Guid value)
     {
         ArgumentNullException.ThrowIfNull(value);
         if (value == Guid.Empty) throw new DomainException("CustomerId cannot be empty");
