49844b7b6d3ab02c842e512d69158229b979d986
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-09 11:54:54 +0300

    Develop Domain Modeling with DDD for Ordering Microservices

---
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -0,0 +1,7 @@
+namespace Ordering.Domain.Models;
+
+public class Product : Entity<Guid>
+{
+    public string Name { get; private set; } = default!;
+    public string Price { get; private set; } = default!;
+}
