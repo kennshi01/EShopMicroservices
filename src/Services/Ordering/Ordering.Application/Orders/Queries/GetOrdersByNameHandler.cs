817dcad14cb102ffa5fc609b33fcc269b2018d3c
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-11 16:20:17 +0300

    Develop Extension method for ProjectToOrderDto use in GetOrders Methods

--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByNameHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByNameHandler.cs
@@ -1,3 +1,5 @@
+using Ordering.Application.Extensions;
+
 namespace Ordering.Application.Orders.Queries;
 
 public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
@@ -13,55 +15,7 @@ public class GetOrdersByNameHandler(IApplicationDbContext dbContext)
             .OrderBy(o => o.OrderName)
             .ToListAsync(cancellationToken);
 
-        // Map to dto
-        var orderDtos = ProjectToOrderDto(orders);
-
         // return result
-        return new GetOrdersByNameResult(orderDtos);
-    }
-
-    private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-    {
-        List<OrderDto> result = new();
-        foreach (var order in orders)
-        {
-            var orderDto = new OrderDto(
-                order.Id.Value,
-                order.CustomerId.Value,
-                order.OrderName.Value,
-                new AddressDto(
-                    order.ShippingAddress.FirstName,
-                    order.ShippingAddress.LastName,
-                    order.ShippingAddress.EmailAddress,
-                    order.ShippingAddress.AddressLine,
-                    order.ShippingAddress.Country,
-                    order.ShippingAddress.State,
-                    order.ShippingAddress.ZipCode
-                ),
-                new AddressDto(
-                    order.BillingAddress.FirstName,
-                    order.BillingAddress.LastName,
-                    order.BillingAddress.EmailAddress,
-                    order.BillingAddress.AddressLine,
-                    order.BillingAddress.Country,
-                    order.BillingAddress.State,
-                    order.BillingAddress.ZipCode
-                ),
-                new PaymentDto(
-                    order.Payment.CardName,
-                    order.Payment.CardNumber,
-                    order.Payment.Expiration,
-                    order.Payment.CVV,
-                    order.Payment.PaymentMethod
-                ),
-                order.Status,
-                order.OrderItems.Select(oi =>
-                    new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
-            );
-
-            result.Add(orderDto);
-        }
-
-        return result;
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
     }
 }
