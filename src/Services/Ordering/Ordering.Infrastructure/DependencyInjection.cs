75f19c3e9bf15efecd6b95b1ad37c65daac253eb
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-11 11:03:44 +0300

    Develop IApplicationDbContext for injection EFCore DB Context object in Ordering

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -21,6 +22,8 @@ public static class DependencyInjection
             options.UseSqlServer(connectionString);
         });
 
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+        
         return services;
     }
 }
