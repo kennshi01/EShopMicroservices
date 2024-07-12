5a9da0a274b90a4fae5be3f41bedb3fef63e33ac
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-12 13:10:13 +0300

    Developing DeleteOrder Endpoint for REST APIs in Ordering.API Layer

--- src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -1,3 +1,5 @@
+using Ordering.Application.Orders.Commands.DeleteOrder;
+
 namespace Ordering.API.Endpoints;
 
 //- Accepts the order id as a parameter.
@@ -5,6 +7,27 @@ namespace Ordering.API.Endpoints;
 //- Uses MediatR to send the command to the corresponding handler
 //- Returns a success or notfound response.
 
-public class DeleteOrder
+//public record DeleteOrderRequest(Guid Id);
+
+public record DeleteOrderResponse(bool IsSuccess);
+
+public class DeleteOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/orders/{id}", async (Guid Id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteOrderCommand(Id));
+
+                var response = result.Adapt<DeleteOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteOrder")
+            .Produces<CreateOrderResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Order")
+            .WithDescription("Delete Order");
+    }
 }
