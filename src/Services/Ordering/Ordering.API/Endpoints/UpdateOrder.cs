53a06f75508c218b4a1e5345d89e459690dd7803
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-12 13:06:49 +0300

    Developing UpdateOrder Endpoint for REST APIs in Ordering.API Layer

--- src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -1,3 +1,5 @@
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
 namespace Ordering.API.Endpoints;
 
 //- Accepts a UpdateOrderRequest object.
@@ -5,6 +7,28 @@ namespace Ordering.API.Endpoints;
 //- Uses MediatR to send the command to the corresponding handler
 //- Returns a success or error response based on the outcome
 
-public class UpdateOrder
+public record UpdateOrderRequest(OrderDto Order);
+
+public record UpdateOrderResponse(bool IsSuccess);
+
+public class UpdateOrder : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/orders", async (UpdateOrderRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateOrderCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateOrderResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateOrder")
+            .Produces<UpdateOrderResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Order")
+            .WithDescription("Update Order");
+    }
 }
