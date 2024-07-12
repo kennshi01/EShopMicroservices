f8fd1b5103ba7918cebb3f8977e9632755a6b6ef
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-12 12:54:02 +0300

    Developing Order Endpoints for REST APIs in Ordering.API Layer

---
+++ src/Services/Ordering/Ordering.API/Endpoints/DeleteOrder.cs
@@ -0,0 +1,10 @@
+namespace Ordering.API.Endpoints;
+
+//- Accepts the order id as a parameter.
+//- Constructs a DeleteOrderCommand
+//- Uses MediatR to send the command to the corresponding handler
+//- Returns a success or notfound response.
+
+public class DeleteOrder
+{
+}
---
+++ src/Services/Ordering/Ordering.API/Endpoints/UpdateOrder.cs
@@ -0,0 +1,10 @@
+namespace Ordering.API.Endpoints;
+
+//- Accepts a UpdateOrderRequest object.
+//- Maps the request to an UpdateOrderCommand
+//- Uses MediatR to send the command to the corresponding handler
+//- Returns a success or error response based on the outcome
+
+public class UpdateOrder
+{
+}
