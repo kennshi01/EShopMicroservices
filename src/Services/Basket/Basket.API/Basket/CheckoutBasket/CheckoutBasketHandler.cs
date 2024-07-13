c52de72eb587d4b48130db698fba99fd83498590
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-13 15:05:24 +0300

    Develop Basket Checkout Handler Method to Publish event to RabbitMQ-MassTransit

--- src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/CheckoutBasket/CheckoutBasketHandler.cs
@@ -1,5 +1,41 @@
+using BuildingBlocks.Messaging.Events;
+using MassTransit;
+
 namespace Basket.API.Basket.CheckoutBasket;
 
-public class CheckoutBasketHandler
+public record CheckoutBasketCommand(BasketCheckoutDto BasketCheckoutDto) : ICommand<CheckoutBasketResult>;
+
+public record CheckoutBasketResult(bool IsSuccess);
+
+public class CheckoutBasketCommandValidator
+    : AbstractValidator<CheckoutBasketCommand>
 {
+    public CheckoutBasketCommandValidator()
+    {
+        RuleFor(x => x.BasketCheckoutDto).NotNull().WithMessage("BasketCheckoutDto should not be null");
+        RuleFor(x => x.BasketCheckoutDto).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class CheckoutBasketHandler(IBasketRepository repository, IPublishEndpoint publishEndpoint)
+    : ICommandHandler<CheckoutBasketCommand, CheckoutBasketResult>
+{
+    public async Task<CheckoutBasketResult> Handle(CheckoutBasketCommand command, CancellationToken cancellationToken)
+    {
+        // get existing basket with total price
+        // Set totalprice on basketcheckout event message
+        var basket = await repository.GetBasket(command.BasketCheckoutDto.UserName, cancellationToken);
+        if (basket is null) return new CheckoutBasketResult(false);
+
+        var eventMessage = command.BasketCheckoutDto.Adapt<BasketCheckoutEvent>();
+        eventMessage.TotalPrice = basket.TotalPrice;
+
+        // send basket checkout event to rabbitmq using masstransit
+        await publishEndpoint.Publish(eventMessage, cancellationToken);
+
+        // delete the basket
+        await repository.DeleteBasket(command.BasketCheckoutDto.UserName, cancellationToken);
+
+        return new CheckoutBasketResult(true);
+    }
 }
