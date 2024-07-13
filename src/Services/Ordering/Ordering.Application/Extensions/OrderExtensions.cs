ec41f52228860268d84501a041536f71f6db8258
VB\eduard.busuioc <eduard.busuioc@vb.md>
2024-07-13 17:06:49 +0300

    Publish OrderCreated Integration event for Order Fullfiment microservices

--- src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -38,4 +38,29 @@ public static class OrderExtensions
                 new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
         ));
     }
+
+    public static OrderDto ToOrderDto(this Order order)
+    {
+        return DtoFromOrder(order);
+    }
+
+    private static OrderDto DtoFromOrder(Order order)
+    {
+        return new OrderDto(
+            order.Id.Value,
+            order.CustomerId.Value,
+            order.OrderName.Value,
+            new AddressDto(order.ShippingAddress.FirstName, order.ShippingAddress.LastName,
+                order.ShippingAddress.EmailAddress!, order.ShippingAddress.AddressLine, order.ShippingAddress.Country,
+                order.ShippingAddress.State, order.ShippingAddress.ZipCode),
+            new AddressDto(order.BillingAddress.FirstName, order.BillingAddress.LastName,
+                order.BillingAddress.EmailAddress!, order.BillingAddress.AddressLine, order.BillingAddress.Country,
+                order.BillingAddress.State, order.BillingAddress.ZipCode),
+            new PaymentDto(order.Payment.CardName!, order.Payment.CardNumber, order.Payment.Expiration,
+                order.Payment.CVV, order.Payment.PaymentMethod),
+            order.Status,
+            order.OrderItems
+                .Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
+        );
+    }
 }
