b0119c6e36b71661700667ab8eb54396dc9dc99d
ebusuioc <edwardbs876@gmail.com>
2024-07-14 23:25:34 +0300

    Bugfix + disabled an check for testing

--- src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -7,17 +7,15 @@ namespace BuildingBlocks.Messaging.MassTransit;
 
 public static class Extensions
 {
-    public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration,
-        Assembly? assembly = null)
+    public static IServiceCollection AddMessageBroker
+        (this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null)
     {
         // Implement RabbitMQ MassTransit configuration
         services.AddMassTransit(config =>
         {
             config.SetKebabCaseEndpointNameFormatter();
-
             if (assembly != null)
                 config.AddConsumers(assembly);
-
             config.UsingRabbitMq((context, configurator) =>
             {
                 configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
@@ -25,6 +23,7 @@ public static class Extensions
                     host.Username(configuration["MessageBroker:UserName"]);
                     host.Password(configuration["MessageBroker:Password"]);
                 });
+                configurator.ConfigureEndpoints(context);
             });
         });
 
